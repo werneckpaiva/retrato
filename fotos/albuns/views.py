c37aa21f69cace1881dd69fed490f4bd2d82384a
Ricardo Paiva <werneck.paiva@gmail.com>
2014-02-12 00:30:05 -0200

    Display django version - hello world

--- fotos/albuns/views.py
+++ fotos/albuns/views.py
@@ -1,8 +1,11 @@
 from django.template import Context, loader
 from django.http import HttpResponse
+import django
 
 
 def index(request):
     t = loader.get_template('albuns/index.html')
-    c = Context({})
+    c = Context({
+        'version': django.get_version()
+    })
     return HttpResponse(t.render(c))
