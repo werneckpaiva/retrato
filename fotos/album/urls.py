b091a2c8780b8aa356b290be2ea88431eca9d8d7
Ricardo Paiva <werneck.paiva@gmail.com>
2014-03-11 20:51:24 -0300

    Increased cache time

--- fotos/album/urls.py
+++ fotos/album/urls.py
@@ -4,5 +4,5 @@ from django.views.decorators.cache import cache_page
 
 
 urlpatterns = patterns('',
-    url(r'^/(?P<album_path>.*)$', cache_page(60)(views.AlbumView.as_view()), name='album')
+    url(r'^/(?P<album_path>.*)$', cache_page(60 * 60 * 24 * 5)(views.AlbumView.as_view()), name='album')
 )
