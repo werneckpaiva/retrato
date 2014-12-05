c13d897d5e6e3c02b1688258262db2aee0c08868
Ricardo Paiva <werneck.paiva@gmail.com>
2014-12-05 21:26:33 -0200

    Fix non admin configuration

--- retrato/settings.py
+++ retrato/settings.py
@@ -53,8 +53,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
     'django.contrib.sessions',
     'django.contrib.contenttypes',
     'django.contrib.messages',
@@ -68,7 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
 #     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
--- retrato/urls.py
+++ retrato/urls.py
@@ -2,13 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.base import RedirectView
 from django.conf import settings
 
-from django.contrib import admin
 # from django.contrib.auth.models import Group
-admin.autodiscover()
 # admin.site.unregister(Group)
 
 urlpatterns = patterns('',
-    url(r'^admin-user/', include(admin.site.urls)),
     url(r'^$', RedirectView.as_view(url='/album/', permanent=True)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
@@ -18,6 +15,9 @@ urlpatterns = patterns('',
 )
 
 if 'retrato.admin' in settings.INSTALLED_APPS:
+    from django.contrib import admin
+    admin.autodiscover()
+    url(r'^admin-user/', include(admin.site.urls)),
     url(r'^admin/$', RedirectView.as_view(url='/admin/album', permanent=True)),
     urlpatterns += patterns('', url(r'^admin/album', include('retrato.admin.album.urls')))
     urlpatterns += patterns('', url(r'^admin/photo', include('retrato.admin.photo.urls')))
